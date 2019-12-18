a27d5a046f2df186bc67d7b030ca7be0f38c1a63
Huang Junye <h.jun.ye@gmail.com>
2019-12-18 14:07:16 +0800

    add pc and key control

--- script.js
+++ script.js
@@ -16,10 +16,12 @@ var game = new Phaser.Game(config);
 
 var cursor;
 var player;
+var pc;
 
 function preloadGame () {
     this.load.image('ground','assets/ground.png');
     this.load.image('player','assets/player.png');
+    this.load.image('pc','assets/pc.png');
 }
 
 function createGame () {
@@ -28,11 +30,26 @@ function createGame () {
     cursor = this.input.keyboard.createCursorKeys();
     console.log(cursor);
 
+    this.keyW=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    this.keyS=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+
     player = this.physics.add.sprite(780, 200, 'player');
     player.setCollideWorldBounds(true);
+
+    pc=this.physics.add.sprite(20, 200, 'pc');
+    pc.setCollideWorldBounds(true);
 }
 
 function updateGame () {
+    // controls for pc
+    if(this.keyW.isDown) {
+        pc.setVelocityY(-150);
+    } else if(this.keyS.isDown) {
+        pc.setVelocityY(150);
+    } else {
+        pc.setVelocityY(0);
+    }
+    // controls for player
     if(cursor.up.isDown) {
         player.setVelocityY(-150);
     } else if(cursor.down.isDown) {
