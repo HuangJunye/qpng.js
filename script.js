c4cd4679a73e5d826bd481cbdc30fdfb07038a0a
Huang Junye <h.jun.ye@gmail.com>
2019-12-18 14:15:18 +0800

    use 2 space tab instead of 4 space tab

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 400,
-    physics: {
-        default: 'arcade'
-    },
-    scene: {
-        preload: preloadGame,
-        create: createGame,
-        update: updateGame
-    }
+  type: Phaser.AUTO,
+  width: 800,
+  height: 400,
+  physics: {
+    default: 'arcade'
+  },
+  scene: {
+    preload: preloadGame,
+    create: createGame,
+    update: updateGame
+  }
 };
 
 var game = new Phaser.Game(config);
@@ -19,54 +19,67 @@ var player;
 var pc;
 var ball;
 
-var velocityX=Phaser.Math.Between(-100, 100);
-var velocityY=100;
+var velocityX = Phaser.Math.Between(-100, 100);
+var velocityY = 100;
 
-function preloadGame () {
-    this.load.image('ground','assets/ground.png');
-    this.load.image('player','assets/player.png');
-    this.load.image('pc','assets/pc.png');
-    this.load.image('ball','assets/ball.png');
+function preloadGame() {
+  this.load.image('ground', 'assets/ground.png');
+  this.load.image('player', 'assets/player.png');
+  this.load.image('pc', 'assets/pc.png');
+  this.load.image('ball', 'assets/ball.png');
 }
 
-function createGame () {
-    this.add.image(400, 200, 'ground');
+function createGame() {
+  this.add.image(400, 200, 'ground');
 
-    cursor = this.input.keyboard.createCursorKeys();
-    console.log(cursor);
+  cursor = this.input.keyboard.createCursorKeys();
+  console.log(cursor);
 
-    this.keyW=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-    this.keyS=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+  this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+  this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
 
-    player = this.physics.add.sprite(780, 200, 'player');
-    player.setCollideWorldBounds(true);
+  player = this.physics.add.sprite(780, 200, 'player');
+  player.setCollideWorldBounds(true);
 
-    pc=this.physics.add.sprite(20, 200, 'pc');
-    pc.setCollideWorldBounds(true);
+  pc = this.physics.add.sprite(20, 200, 'pc');
+  pc.setCollideWorldBounds(true);
 
-    ball = this.physics.add.sprite(400, 200, 'ball');
-    ball.setCollideWorldBounds(true);
-    ball.setBounce(1);
+  ball = this.physics.add.sprite(400, 200, 'ball');
+  ball.setCollideWorldBounds(true);
+  ball.setBounce(1);
+  ball.setVelocityY(velocityY);
+  ball.setVelocityX(velocityX);
+}
+
+function updateGame() {
+  // controls for pc
+  if (this.keyW.isDown) {
+    pc.setVelocityY(-150);
+  } else if (this.keyS.isDown) {
+    pc.setVelocityY(150);
+  } else {
+    pc.setVelocityY(0);
+  }
+  // controls for player
+  if (cursor.up.isDown) {
+    player.setVelocityY(-150);
+  } else if (cursor.down.isDown) {
+    player.setVelocityY(150);
+  } else {
+    player.setVelocityY(0);
+  }
+}
+
+function hitPlayer(ball, player) {
+  velocityX = velocityX + 50;
+  velocityX = velocityX * -1;
+  console.log(velocityX);
+
+  ball.setVelocityX(velocityX);
+
+  if (velocityY < 0) {
+    velocityY = velocityY * -1
     ball.setVelocityY(velocityY);
-    ball.setVelocityX(velocityX);
+  }
+  player.setVelocityX(-1);
 }
-
-function updateGame () {
-    // controls for pc
-    if(this.keyW.isDown) {
-        pc.setVelocityY(-150);
-    } else if(this.keyS.isDown) {
-        pc.setVelocityY(150);
-    } else {
-        pc.setVelocityY(0);
-    }
-    // controls for player
-    if(cursor.up.isDown) {
-        player.setVelocityY(-150);
-    } else if(cursor.down.isDown) {
-        player.setVelocityY(150);
-    } else {
-        player.setVelocityY(0);
-    }
-}
-
